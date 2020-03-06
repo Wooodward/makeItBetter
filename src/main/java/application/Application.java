a43a29ccb0d54d8c5e93dc0a27652b930122862b
wooodward77 <wooodward77@gmail.com>
2020-03-06 21:57:49 +0300

    Initial commit

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,20 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String root(){
+        return "This is the root page";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
